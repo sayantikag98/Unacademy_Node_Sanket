d6ee647d0faa0a736ab825adae99ad377c2d7e57
Sayantika Ghosh <sayantikaghosh98@gmail.com>
2021-11-20 12:14:21 +0530

    Lec-5 custom middleware and express router added

--- L4/webserver_with_express/src/server.js
+++ L4/webserver_with_express/src/server.js
@@ -4,14 +4,26 @@ import { json, urlencoded } from "body-parser";
 
 const app = express();
 const PORT = 3000;
+const router = express.Router();
 
 
 app.use(morgan("dev"));
-// app.use(express.json());
 app.use(json());
 app.use(urlencoded({extended:true}));
+app.use("/posts",router);
 
-app.get("/", (req, res) => {
+
+// Custom middleware
+const customMiddleWareFunc = (req, res, next) => {
+    console.log("Custom middleware created");
+    console.log("Custom middleware");
+    console.log(req.body, req.params, res.json.msg);
+    next();
+};
+
+app.get("/", customMiddleWareFunc, (req, res) => {
+    console.log("Call back called");
+    console.log(req.body, req.params, res.json.msg);
     // res.send("Hello World Sayantika");
     res.json({
         msg:"Get Request"
@@ -23,6 +35,32 @@ app.post("/", (req, res) => {
     res.json({
         msg: "Post request send..."
     });
+});
+
+router
+.route("/")
+.get((req, res) => {
+    res.send("Posts route get");
+})
+.post((req, res) => {
+    console.log(req.body);
+    res.send("Posts route post")
+});
+
+router
+.route("/:id/:num")
+.patch(customMiddleWareFunc, (req, res) => {
+    console.log("Patch request");
+    console.log(req.body, req.params);
+    res.send("Posts route patch");
+})
+.put((req, res) => {
+    console.log(req.body, req.params);
+    res.send("Posts route put");
+})
+.delete((req, res) => {
+    console.log(req.body, req.params);
+    res.send("Posts route delete");
 });
 
 export const startFunc = () => {
